230e74fb4d46a7f599f8199450324e559e59fc2a
dal <dal.adamson@gmail.com>
2014-09-08 18:30:33 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plonkers::Application.config.secret_key_base = '6a56b3f310d61e980001269285f01b6c435484fb85c7780585d6299388fc9f956b23d05d77dc2086b14868c8af36931095f0f9c409933cdc335af0f47209c86f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Plonkers::Application.config.session_store :cookie_store, key: '_plonkers_session'
